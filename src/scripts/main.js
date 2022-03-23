d7869841bf1fa3ffa77055f427ce07c6e304bb75
Noah Winkler <noahwinkler3700@icloud.com>
2022-03-23 10:46:29 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Main logic module for what should happen on initial page load for Giffygram
+ */
+
+/*
+    This function performs one, specific task.
+
+    1. Can you explain what that task is?
+    2. Are you defining the function here or invoking it?
+*/
+
+
+const startGiffyGram = () => {
+    const postElement = document.querySelector(".postList");
+	postElement.innerHTML = "Hello Cohort 47"
+}
+// Are you defining the function here or invoking it?
+startGiffyGram();
